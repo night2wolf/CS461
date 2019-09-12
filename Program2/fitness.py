c68dc5d90d035931feb9e4faeb76e471a29033c1
Trevor Klinkenberg <tklinkenberg@scriptpro.com>
2019-09-12 18:17:27 -0500

    Fitness update
    multiple parts of fitness evaluation done.

--- Program2/fitness.py
+++ Program2/fitness.py
@@ -24,62 +24,70 @@ import random
 def create_schedule():
     random_schedule = []
     for i in range(len(data.classes)):
-        class_schedule = data.Schedule
-        class_schedule.clas = data.classes[i]
-        class_schedule.time = random.choice(data.times)
-        class_schedule.instructor = random.choice(data.instructors)
-        class_schedule.location = random.choice(data.locations)
+        class_schedule = data.Schedule(data.classes[i],random.choice(data.times),random.choice(data.instructors),random.choice(data.locations))
         random_schedule.append(class_schedule)
         print(str(class_schedule.clas) + " : " + 
               str(class_schedule.time) + " : " + 
               str(class_schedule.instructor) + " : " + 
-              str(class_schedule.location) + '\n')
+              str(class_schedule.location))
     return random_schedule
 def fitness_evaluate(schedule):
     score = 0
     # For each course that is taught by an instructor who can teach it: +3
-    for i in range(len(schedule)):
-        print(str(schedule[i].clas) + " : " + str(schedule[i].instructor) + '\n' + str(score))
+    for i in range(len(schedule)):        
         for j in range(len(data.Hare_classes)):
             if schedule[i].clas == data.Hare_classes[j] and schedule[i].instructor == "Hare":
                 score = score + 3
-                """
         for k in range(len(data.Bingham_classes)):
-            if schedule[i].clas == data.Bingham_classes[k]:
+            if schedule[i].clas == data.Bingham_classes[k] and schedule[k].instructor == "Bingham":
                 score = score + 3
         for l in range(len(data.Kuhail_classes)):
-            if schedule[i].clas == data.Kuhail_classes[l]:
+            if schedule[i].clas == data.Kuhail_classes[l] and schedule[l].instructor == "Kuhail":
                 score = score + 3
         for m in range(len(data.Mitchell_classes)):
-            if schedule[i].clas == data.Mitchell_classes[m]:
+            if schedule[i].clas == data.Mitchell_classes[m] and schedule[m].instructor == "Mitchell":
                 score = score + 3
         for n in range(len(data.Rao_classes)):
-            if schedule[i].clas == data.Rao_classes[n]:
-                score = score + 3
+            if schedule[i].clas == data.Rao_classes[n] and schedule[n].instructor == "Rao":
+                score = score + 3        
+        # For each course that is the only course scheduled in that room at that time: +5
+        counter = 0
+        for j in range(len(schedule)):
+            if schedule[i].time == schedule[j].time and schedule[i].location == schedule[j].location:
+                # count each instance of a conflict
+                counter = counter + 1                
+        # If the counter is only 1 that means it is by itself and we give a reward
+        if counter == 1:
+            score = score + 5     
+        """         
+        # For each course that is in a room large enough to accommodate it: +5
+    
+        # Room capacity is no more than twice the expected enrollment: +2
         """
-    # For each course that is the only course scheduled in that room at that time: +5
-
-    # For each course that is in a room large enough to accommodate it: +5
-
-    # Room capacity is no more than twice the expected enrollment: +2
-
-    # For each course that does not have the same instructor teaching another course at the same time: +5
-
+        # For each course that does not have the same instructor teaching another course at the same time: +5
+        counter = 0
+        for j in range(len(schedule)):
+            if schedule[i].time == schedule[j].time and schedule[i].instructor == schedule[j].instructor:
+                # count each instance of a conflict
+                counter = counter + 1
+        # If the counter is only 1 that means it is by itself and we give a reward
+        if counter == 1:
+            score = score + 5
+    
     # For each schedule that has the same instructor teaching more than 4 courses: -5 per course over 4
-
+    
+    """
     # For each schedule that has Rao or Mitchell(graduate faculty) teaching more courses than Hare or Bingham: -5 % to total fitness score. (Same number of courses is OK.)
-
+    """
     return score
 sch = create_schedule()
-print(str(sch[0].clas) + " : " + str(sch[0].instructor) + '\n')
-print(str(sch[1].clas) + " : " + str(sch[1].instructor) + '\n')
-print(str(sch[2].clas) + " : " + str(sch[2].instructor) + '\n')
 #for i in range(len(sch)):
 #    print(str(sch[i].clas) + " : " + str(sch[i].instructor) + '\n')
-#score = fitness_evaluate(sch)
-#print(str(score))
-"""
+score = fitness_evaluate(sch)
+print(str(score))
+
 # Create a generation of 1000 random schedules 
+"""
 generation = []
 for i in range(1000):
     yeet = create_schedule()
