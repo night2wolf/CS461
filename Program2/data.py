4a1b3bb3f21fa976a6a8362b2fb17cc0e4ebf8f4
Trevor Klinkenberg <tklinkenberg@scriptpro.com>
2019-09-24 08:56:06 -0500

    Working on crossover / Mutation

--- Program2/data.py
+++ Program2/data.py
@@ -1,9 +1,11 @@
 classes = ["CS101A", "CS101B", "CS201A", "CS201B", "CS191A", "CS191B", "CS291B",
            "CS291A", "CS303", "CS341", "CS449", "CS461"]
+class_size = [40,25,30,30,60,20,40,20,50,40,55,40]
 times = ['10A', '11A', '12P', '1P', '2P', '3P', '4P']
 # Note: May need to do some string manipulation to get only the building and exclude room #
 locations = ['Haag301', 'Haag206', 'Royall204', 'Katz209',
              'Flarsheim310', 'Flarsheim260', 'Bloch0009']
+location_size = [70,30,70,50,80,25,30]
 # Note: May need external logic to evaluate if a teacher can teach a class
 instructors = ['Hare', 'Bingham', 'Kuhail', 'Mitchell', 'Rao']
 # Lazy method
@@ -20,8 +22,10 @@ Rao_classes = ['CS291A', 'CS291B', 'CS303', 'CS341', 'CS461']
 
 
 class Schedule():
-    def __init__(self, clas, time, instructor, location):
+    def __init__(self, clas,class_size, time, instructor, location,location_size):
         self.clas = clas
+        self.class_size = class_size
         self.time = time
         self.instructor = instructor
         self.location = location
+        self.location_size = location_size
--- Program2/fitness.py
+++ Program2/fitness.py
@@ -20,19 +20,47 @@ if these courses are scheduled for adjacent times, and are in the same building:
 import data
 import random
 
+def determine_location_size(location):
+    location_size = 0
+    if location == 'Haag301':
+        location_size = 70
+    if location == 'Haag206':
+        location_size = 30
+    if location == 'Royall204':
+        location_size = 70
+    if location == 'Katz209':
+        location_size = 50
+    if location == 'Flarsheim310':
+        location_size = 80
+    if location == 'Flarsheim260':
+        location_size = 25
+    if location == 'Bloch0009':
+        location_size = 30                
+    return location_size
+
 #Create a Random Schedule using all the classes
 def create_schedule():
     random_schedule = []
     for i in range(len(data.classes)):
-        class_schedule = data.Schedule(data.classes[i],random.choice(data.times),random.choice(data.instructors),random.choice(data.locations))
+        location = random.choice(data.locations)
+        class_schedule = data.Schedule(data.classes[i],data.class_size[i],random.choice(data.times),random.choice(data.instructors),location,determine_location_size(location))
         random_schedule.append(class_schedule)
+        """
         print(str(class_schedule.clas) + " : " + 
+              str(class_schedule.class_size) + " : " +  
               str(class_schedule.time) + " : " + 
               str(class_schedule.instructor) + " : " + 
-              str(class_schedule.location))
+              str(class_schedule.location) + " : " +
+              str(class_schedule.location_size))
+        """
     return random_schedule
 def fitness_evaluate(schedule):
     score = 0
+    Hare_counter = 0
+    Bingham_counter = 0
+    Kuhail_counter = 0
+    Mitchell_counter = 0
+    Rao_counter = 0
     # For each course that is taught by an instructor who can teach it: +3
     for i in range(len(schedule)):        
         for j in range(len(data.Hare_classes)):
@@ -61,9 +89,19 @@ def fitness_evaluate(schedule):
             score = score + 5     
         """         
         # For each course that is in a room large enough to accommodate it: +5
-    
-        # Room capacity is no more than twice the expected enrollment: +2
+        Haag 301 (70), Haag 206 (30), Royall 204 (70), Katz 209 (50),
+        Flarsheim 310 (80), Flarsheim 260 (25), Bloch 0009 (30)
         """
+        if schedule[i].location_size >= schedule[i].class_size:
+            score = score + 5
+        """
+        # Room capacity is no more than twice the expected enrollment: +2
+        CS 101A (40), CS 101B (25), CS 201A (30), CS 201B (30), CS 191A (60),
+        CS 191B (20), CS 291B (40), CS 291A (20), CS 303 (50), CS 341 (40), CS 449 (55), CS 461 (40).
+        """
+        enrollment = schedule[i].class_size * 2
+        if enrollment >= schedule[i].location_size:
+            score = score +2
         # For each course that does not have the same instructor teaching another course at the same time: +5
         counter = 0
         for j in range(len(schedule)):
@@ -73,24 +111,43 @@ def fitness_evaluate(schedule):
         # If the counter is only 1 that means it is by itself and we give a reward
         if counter == 1:
             score = score + 5
-    
+        # Keep a running count of who is teaching
+        if schedule[i].instructor == "Hare" :
+            Hare_counter = Hare_counter + 1
+        if schedule[i].instructor == "Bingham" :
+            Bingham_counter = Bingham_counter + 1
+        if schedule[i].instructor == "Kuhail":
+            Kuhail_counter = Kuhail_counter + 1
+        if schedule[i].instructor == "Mitchell" :
+            Mitchell_counter = Mitchell_counter + 1
+        if schedule[i].instructor == "Rao":
+            Rao_counter = Rao_counter + 1    
+           
     # For each schedule that has the same instructor teaching more than 4 courses: -5 per course over 4
+    if Hare_counter > 4:
+        score = score - (5*(Hare_counter-4))
+    if Bingham_counter > 4:
+        score = score - (5*(Bingham_counter-4))
+    if Kuhail_counter > 4:
+        score = score - (5*(Kuhail_counter-4))
+    if Mitchell_counter > 4:
+        score = score - (5*(Mitchell_counter-4))
+    if Rao_counter > 4:
+        score = score - (5*(Rao_counter-4))
     
-    """
     # For each schedule that has Rao or Mitchell(graduate faculty) teaching more courses than Hare or Bingham: -5 % to total fitness score. (Same number of courses is OK.)
-    """
+    Mitchell_Rao_courses = Mitchell_counter + Rao_counter
+    Hare_Bingham_courses = Hare_counter + Bingham_counter    
+    if Mitchell_Rao_courses > Hare_Bingham_courses:
+        score = score * .95
+
     return score
+
+"""    
 sch = create_schedule()
 #for i in range(len(sch)):
 #    print(str(sch[i].clas) + " : " + str(sch[i].instructor) + '\n')
 score = fitness_evaluate(sch)
 print(str(score))
+"""
 
-# Create a generation of 1000 random schedules 
-"""
-generation = []
-for i in range(1000):
-    yeet = create_schedule()
-    generation.append(yeet)
-print(str(generation))    
-"""
